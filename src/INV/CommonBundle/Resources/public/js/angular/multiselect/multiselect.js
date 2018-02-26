ef9e22869507d354e66dad69c78f27a030e0efe1
Felipe Rdguez Arias <farias2014@nauta.cu>
2018-02-26 15:07:59 -0500

    adding TwigAsset var for assets de angular

--- src/INV/CommonBundle/Resources/public/js/angular/multiselect/multiselect.js
+++ src/INV/CommonBundle/Resources/public/js/angular/multiselect/multiselect.js
@@ -218,7 +218,7 @@ angular.module('ui.multiselect', [])
       scope: false,
       replace: true,
       // templateUrl: 'multiselect.tmpl.html',
-      templateUrl: '/bundles/common/js/angular/multiselect/multiselect.tmpl.html',
+      templateUrl: TwigAsset.multiselect,
       link: function (scope, element, attrs) {
 
         scope.isVisible = false
