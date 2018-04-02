d6d3d3ce01786f6234aa6b266d4085dd090267e7
Felipe Rdguez Arias <farias2014@nauta.cu>
2018-04-02 13:09:13 -0400

    cambios menores y base de datos del dia de abril

--- src/INV/FrontendBundle/Resources/public/js/iApp/controller/ByApunteController.js
+++ src/INV/FrontendBundle/Resources/public/js/iApp/controller/ByApunteController.js
@@ -5,33 +5,33 @@
 iApp.controller('ByApunteController',
   function ($scope) {
 
-    $scope.activos = TwigParams.activos
-    $scope.original = TwigParams.activos
-    $scope.selected = []
-    $scope.sValue = ''
+    $scope.activos = TwigParams.activos;
+    $scope.original = TwigParams.activos;
+    $scope.selected = [];
+    $scope.sValue = '';
 
     $scope.activos = $scope.activos.map(activo => {
-        activo.badge = activo.cantidad > 0 ? `badge-info ${activo.id}` : `badge-danger ${activo.id}`
-        activo.selected = ''
-        return activo
+        activo.badge = activo.cantidad > 0 ? `badge-info ${activo.id}` : `badge-danger ${activo.id}`;
+        activo.selected = '';
+        return activo;
       }
-    )
+    );
 
     $scope.getUrlControl = (activo) => {
-      return Routing.generate('apunte_new_control', {'idActivo': activo.id})
-    }
+      return Routing.generate('apunte_new_control', {'idActivo': activo.id});
+    };
 
     $scope.rowSelect = (activo) => {
       $scope.activos = $scope.activos.map(act => {
         if (act.id === activo.id) {
-          act.selected = activo.selected === '' ? 'selected' : ''
+          act.selected = activo.selected === '' ? 'selected' : '';
         }
-        return act
-      })
+        return act;
+      });
 
       $scope.selected = $scope.activos.filter(act => act.selected === 'selected');
       $scope.sValue = JSON.stringify($scope.selected);
-    }
+    };
 
   }
-)
+);
